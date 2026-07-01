1fbdc6d96dbaa831898a16759c4d7ad672b60626
Matheus Silva <rematheussilva@gmail.com>
2026-07-01 16:27:50 -0500

    feat: add implement the onSubmit method for the form

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+
+  function onSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    alert("Enviado!");
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
-      <Input required legend="Senha" type="password" placeholder="12345" />
-      <Button>Entrar</Button>
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="12345"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button isLoading type="submit">
+        Entrar
+      </Button>
     </form>
   );
 }
