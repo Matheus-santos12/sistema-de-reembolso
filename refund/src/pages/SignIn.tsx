dedf40df10b1247b7bb30063835975c0f8bf4654
Matheus Silva <rematheussilva@gmail.com>
2026-07-01 16:35:39 -0500

    feat: add a link that redirects to account creation

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -31,9 +31,14 @@ export function SignIn() {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Button isLoading type="submit">
-        Entrar
-      </Button>
+      <Button type="submit">Entrar</Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-8 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
     </form>
   );
 }
