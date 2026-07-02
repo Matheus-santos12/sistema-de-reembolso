6cee34fa9090c20cbc102736e7fb15ef53201452
Matheus Silva <rematheussilva@gmail.com>
2026-07-02 15:50:35 -0500

    feat: add a header component to the application

---
+++ refund/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import logoSvg from "../assets/logo.svg";
+import logoutSvg from "../assets/logout.svg";
+
+export function Header() {
+  return (
+    <header className="w-full flex justify-between ">
+      <img src={logoSvg} alt="Logo" />
+
+      <div className=" flex items-center gap-3">
+        <span className=" text-sm font-semibold text-gray-200 ">
+          Olá, Matheus
+        </span>
+
+        <img
+          src={logoutSvg}
+          alt="Ícone de sair"
+          className=" my-8 cursor-pointer hover:opacity-60 transition ease-linear "
+        />
+      </div>
+    </header>
+  );
+}
