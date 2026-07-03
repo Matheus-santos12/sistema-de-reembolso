3295c23f98554cfb1ecd7c9284baea664470409d
Matheus Silva <rematheussilva@gmail.com>
2026-07-03 16:07:41 -0500

    feat: add a submit button and its functionality

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
 export function Refund() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  const [filename, setFilename] = useState<File | null>(null);
+
+  function onSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+    >
       <header>
         <h1 className=" text-xl font-bold text-gray-100 ">
           Solicitação de reembolso
@@ -18,7 +31,12 @@ export function Refund() {
         </p>
       </header>
 
-      <Input required legend="Nome da solicitação" />
+      <Input
+        required
+        legend="Nome da solicitação"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <div className="flex gap-4">
         <Select
@@ -34,10 +52,22 @@ export function Refund() {
           ))}
         </Select>
 
-        <Input legend="Valor" required />
+        <Input
+          legend="Valor"
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
 
-      <Upload filename="matheus.sgv" />
+      <Upload
+        filename={filename && filename.name}
+        onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Enviar
+      </Button>
     </form>
   );
 }
