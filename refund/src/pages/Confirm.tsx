997f21ccf45f3a45915de1b0342fb0eb07cd1a41
Matheus Silva <rematheussilva@gmail.com>
2026-07-03 16:48:30 -0500

    feat: add confirmation page interface

--- refund/src/pages/Confirm.tsx
+++ refund/src/pages/Confirm.tsx
@@ -1,4 +1,5 @@
 import { Navigate, useLocation } from "react-router";
+import okSvg from "../assets/ok.svg";
 
 export function Confirm() {
   const location = useLocation();
@@ -6,5 +7,23 @@ export function Confirm() {
   if (!location.state?.fromSubmit) {
     return <Navigate to="/" />;
   }
-  return <h1>Confirm</h1>;
+  return (
+    <div className="bg-gray-500 lf-w:[512px] rounded-xl flex flex-col items-center p-10 gap-6 ">
+      <h1>Solicitação enviada</h1>
+
+      <img src={okSvg} alt="Ícone de ok" className="" />
+
+      <p className=" text-sm text-gray-200 text-center ">
+        Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o
+        setor financeiro irá entrar em contato com você
+      </p>
+
+      <a
+        href="/"
+        className=" w-full p-3 text-center bg-green-100 rounded-xl text-white hover:bg-green-200 transition ease-linear "
+      >
+        Nova solicitação
+      </a>
+    </div>
+  );
 }
