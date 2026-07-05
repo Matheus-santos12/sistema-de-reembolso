c9c1722fa90c652672451c808b25c9128543c9aa
Matheus Silva <rematheussilva@gmail.com>
2026-07-04 22:03:13 -0500

    feat: add button variants

--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import searchSvg from "../assets/search.svg";
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function Dashboard() {
@@ -6,14 +8,26 @@ export function Dashboard() {
 
   function fetchRefunds(e: React.SubmitEvent) {
     e.preventDefault();
+
+    console.log(name);
   }
 
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
-      <form className=" flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6 ">
-        <Input onSubmit={fetchRefunds} placeholder="Pesquisar pelo nome" />
+      <form
+        onSubmit={fetchRefunds}
+        className=" flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6 "
+      >
+        <Input
+          placeholder="Pesquisar pelo nome"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        </Button>
       </form>
     </div>
   );
