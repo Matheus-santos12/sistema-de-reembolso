53a5628019a3d17034a1181a581d1e12d65b156e
Matheus Silva <rematheussilva@gmail.com>
2026-07-06 17:42:14 -0500

    feat: add page navigation component

--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import searchSvg from "../assets/search.svg";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Pagination } from "../components/Pagination";
 import { RefundItem } from "../components/RefundItem";
 import { CATEGORIES } from "../utils/categories";
 import { formatCurrency } from "../utils/formatCurrency";
@@ -44,6 +45,8 @@ export function Dashboard() {
       <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll ">
         <RefundItem data={REFUND_EXAMPLE} />
       </div>
+
+      <Pagination current={1} total={10} />
     </div>
   );
 }
