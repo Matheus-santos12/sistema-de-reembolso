8a2873189938731d7ec64324baf7675bb369a2a8
Matheus Silva <rematheussilva@gmail.com>
2026-07-08 14:24:32 -0500

    feat: add forms validation implementation using zod with axios

--- refund/src/pages/SignUp.tsx
+++ refund/src/pages/SignUp.tsx
@@ -1,18 +1,49 @@
 import { useState } from "react";
+import { z, ZodError } from "zod";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve conter no mínimo 6 dígitos" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function onSubmit(e: React.SubmitEvent) {
     e.preventDefault();
 
-    alert("Enviado!");
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+      alert("Não foi possível cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
