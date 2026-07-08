ec79d0f2d14fb1b91332a936b03f8c7fdecdc4ec
Matheus Silva <rematheussilva@gmail.com>
2026-07-08 15:42:18 -0500

    feat: add data validation with zod

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -1,18 +1,33 @@
 import { useActionState } from "react";
+import z, { ZodError } from "zod";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
+const signInScheme = z.object({
+  email: z.email({ message: "E-mail inválido" }),
+  password: z.string().trim().min(1, { message: "Informe a senha" }),
+});
+
 export function SignIn() {
-  const [state, formAction, isLoading] = useActionState(signIn, {
-    email: "",
-    password: "",
-  });
+  const [state, formAction, isLoading] = useActionState(signIn, null);
 
-  async function signIn(prevState: any, formData: FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
+  async function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInScheme.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
 
-    return { email, password };
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message };
+      }
+
+      return { message: "Não foi possível entrar!" };
+    }
   }
 
   return (
@@ -23,7 +38,6 @@ export function SignIn() {
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        defaultValue={String(state?.email)}
       />
 
       <Input
@@ -32,8 +46,11 @@ export function SignIn() {
         legend="Senha"
         type="password"
         placeholder="12345"
-        defaultValue={String(state?.password)}
       />
+
+      <p className=" text-sm text-red-600 text-center my-4 font-medium ">
+        {state?.message}
+      </p>
 
       <Button type="submit" isLoading={isLoading}>
         Entrar
