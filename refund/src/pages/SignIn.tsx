44f40954b196dfe5a93fee7db02bd0250514ccd8
Matheus Silva <rematheussilva@gmail.com>
2026-07-10 13:56:34 -0500

    add a function to save the data returned by the api to the state

--- refund/src/pages/SignIn.tsx
+++ refund/src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import { useActionState } from "react";
 import z, { ZodError } from "zod";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { useAuth } from "../hooks/useAuth";
 import { api } from "../services/api";
 
 const signInScheme = z.object({
@@ -13,6 +14,8 @@ const signInScheme = z.object({
 export function SignIn() {
   const [state, formAction, isLoading] = useActionState(signIn, null);
 
+  const auth = useAuth();
+
   async function signIn(_: any, formData: FormData) {
     try {
       const data = signInScheme.parse({
@@ -21,7 +24,7 @@ export function SignIn() {
       });
 
       const response = await api.post("/sessions", data);
-      console.log(response.data);
+      auth.save(response.data);
     } catch (error) {
       console.log(error);
 
--- refund/src/routes/index.tsx
+++ refund/src/routes/index.tsx
@@ -7,18 +7,11 @@ import { ManagerRoutes } from "./ManagerRoutes";
 
 const isLoading = false;
 
-const session = {
-  user: {
-    role: "",
-  },
-};
-
 export function Routes() {
-  const context = useAuth();
-  console.log(context);
+  const { session } = useAuth();
 
   function Route() {
-    switch (session.user.role) {
+    switch (session?.user.role) {
       case "employee":
         return <EmployeeRoutes />;
       case "manager":
