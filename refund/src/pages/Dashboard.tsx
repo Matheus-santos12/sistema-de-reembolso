7f631a3c81e13ba7c26164793e7540b4116a207b
Matheus Silva <rematheussilva@gmail.com>
2026-07-13 14:50:33 -0500

    feat: add api data display

--- refund/src/pages/Dashboard.tsx
+++ refund/src/pages/Dashboard.tsx
@@ -9,21 +9,13 @@ import { api } from "../services/api";
 import { CATEGORIES } from "../utils/categories";
 import { formatCurrency } from "../utils/formatCurrency";
 
-const REFUND_EXAMPLE = {
-  id: "1234",
-  name: "Matheus",
-  category: "Transport",
-  amount: formatCurrency(32.5),
-  categoryImg: CATEGORIES["transport"].icon,
-};
-
 const PER_PAGE = 5;
 
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
   const [totalOfPage, setTotalOfPage] = useState(0);
-  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([]);
 
   async function fetchRefunds() {
     try {
@@ -31,7 +23,17 @@ export function Dashboard() {
         `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`,
       );
 
-      console.log(response.data);
+      setRefunds(
+        response.data.refunds.map((refund) => ({
+          id: refund.id,
+          name: refund.user.name,
+          description: refund.name,
+          amount: formatCurrency(refund.amount),
+          categoryImg: CATEGORIES[refund.category].icon,
+        })),
+      );
+
+      setTotalOfPage(response.data.pagination.totalPages);
     } catch (error) {
       console.log(error);
 
