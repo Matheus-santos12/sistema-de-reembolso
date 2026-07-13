92593244a94c344cc682f70bd916784b479e854b
Matheus Silva <rematheussilva@gmail.com>
2026-07-13 13:57:58 -0500

    feat: add file upload flow

--- refund/src/pages/Refund.tsx
+++ refund/src/pages/Refund.tsx
@@ -25,7 +25,7 @@ export function Refund() {
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [filename, setFilename] = useState<File | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -40,6 +40,15 @@ export function Refund() {
     try {
       setIsLoading(true);
 
+      if (!file) {
+        return alert("Selecione um arquivo de comprovante");
+      }
+
+      const fileUploadForm = new FormData();
+      fileUploadForm.append("file", file);
+
+      const response = await api.post("/uploads", fileUploadForm);
+
       const data = refundSchema.parse({
         name,
         category,
@@ -48,7 +57,7 @@ export function Refund() {
 
       await api.post("/refunds", {
         ...data,
-        filename: "9489849284952975t4.png",
+        filename: response.data.filename,
       });
 
       navigate("/confirm", { state: { fromSubmit: true } });
@@ -126,8 +135,8 @@ export function Refund() {
         </a>
       ) : (
         <Upload
-          filename={filename && filename.name}
-          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+          filename={file && file.name}
+          onChange={(e) => e.target.files && setFile(e.target.files[0])}
         />
       )}
 
